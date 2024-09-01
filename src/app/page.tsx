c854a6b896e2a1d675d879741abd37e4df94bc97
tomoya-kamaji <tomoya.kamaji@gmail.com>
2024-09-01 17:37:42 +0900

    hono

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to the User API</h1>
-    </div>
-  );
-};
+"use client";
 
-export default Home;
+import { useEffect, useState } from "react";
+import { hc } from "hono/client";
+import type { AppType } from "./api/[...route]/route";
+
+const client = hc<AppType>("/");
+
+export default function Home() {
+  const [name, setName] = useState<string>();
+
+  useEffect(() => {
+    client.api.hello
+      .$get()
+      .then((res) => res.json())
+      .then((json) => setName(json.name));
+  }, []);
+
+  return <p>{typeof name !== "undefined" ? `Hello ${name}!` : "Loading..."}</p>;
+}
