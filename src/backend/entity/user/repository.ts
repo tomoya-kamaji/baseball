5575a96089e005114acb491cef105c5e3782eab1
tomoya-kamaji <tomoya.kamaji@gmail.com>
2024-09-09 08:26:53 +0900

    関数型

--- src/backend/entity/user/repository.ts
+++ src/backend/entity/user/repository.ts
@@ -1,1 +1,48 @@
-// userRepository.ts
+import { PrismaClient, UserStatus } from '@prisma/client'
+import { ResultAsync, ok } from 'neverthrow'
+import { User, InitUser, ActiveUser, isInitUser, isActiveUser } from './user'
+
+const prisma = new PrismaClient()
+
+const findUserById = (id: string): ResultAsync<User, Error> => {
+  return ResultAsync.fromPromise(
+    prisma.user.findUniqueOrThrow({ where: { id } }),
+    () => new Error('PrismaClientError')
+  ).andThen((user) => {
+    if (user.status === UserStatus.INIT) {
+      return ok({
+        id: user.id,
+        name: user.name,
+        kind: 'init'
+      } as InitUser)
+    } else {
+      return ok({
+        id: user.id,
+        name: user.name,
+        kind: 'active',
+        teamId: '1'
+      } as ActiveUser)
+    }
+  })
+}
+
+const createUser = (user: InitUser): ResultAsync<InitUser, Error> => {
+  return ResultAsync.fromPromise(
+    prisma.user.create({
+      data: {
+        id: user.id,
+        name: user.name,
+        status: UserStatus.INIT
+      }
+    }),
+    () => new Error('PrismaClientError')
+  ).map((createdUser) => {
+    return {
+      id: createdUser.id,
+      name: createdUser.name,
+      kind: 'init'
+    }
+  })
+}
+
+export { findUserById, createUser }
--- src/backend/entity/user/user.ts
+++ src/backend/entity/user/user.ts
@@ -1,14 +1,38 @@
-type ActiveUser = {
-  id: number
+import { Result, err, ok } from 'neverthrow'
+
+type InitUser = {
+  kind: 'init'
+  id: string
   name: string
 }
 
-type InitUser = {
-  id: number
+type ActiveUser = {
+  kind: 'active'
+  id: string
   name: string
   teamId: string
 }
-
 type User = ActiveUser | InitUser
 
-export type { User, ActiveUser, InitUser }
+const isInitUser = (user: User): user is InitUser => {
+  return user.kind === 'init'
+}
+
+const isActiveUser = (user: User): user is ActiveUser => {
+  return user.kind === 'active'
+}
+
+const joinTeam = (user: InitUser, teamId: string): Result<ActiveUser, Error> => {
+  if (teamId.length > 5) {
+    return err(new Error('チームIDは5文字以下である必要があります'))
+  }
+
+  return ok({
+    kind: 'active',
+    id: user.id,
+    name: user.name,
+    teamId: teamId
+  })
+}
+
+export { isInitUser, isActiveUser, type User, type InitUser, type ActiveUser, joinTeam }
